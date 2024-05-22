d59bd1539841cdd7b2c95727a5459e760228f89f
DarksideEvil <sinister58450@gmail.com>
2024-05-22 11:36:56 +0500

    intial && last commit

--- src/settings/security/crossOrigin.js
+++ src/settings/security/crossOrigin.js
@@ -2,12 +2,12 @@ const cors = require("cors");
 
 const corsOptions = {
   origin: "https://charge-mate-client.vercel.app",
-  methods: "GET,HEAD,OPTIONS,POST,PUT,PATCH",
+  methods: "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE", // Include DELETE method
   allowedHeaders:
     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
 };
 
 module.exports = function secureApp(app) {
   app.use(cors(corsOptions));
-  app.options("*", cors(corsOptions)); // Pre-flight requests
+  app.options("*", cors()); // Allow preflight requests for all routes
 };
